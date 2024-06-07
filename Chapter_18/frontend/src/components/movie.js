a2ed39aeab51bb7e71d99870a2df6b0bb812749f
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-06-07 12:38:48 +0200

    Mern

--- Chapter_18/frontend/src/components/movie.js
+++ Chapter_18/frontend/src/components/movie.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import MovieDataService from "../services/movies";
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Media from "react-bootstrap/Media";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({
@@ -9,7 +16,7 @@ const Movie = (props) => {
     rated: "",
     reviews: [],
   });
-  
+
   const getMovie = (id) => {
     MovieDataService.get(id)
       .then((response) => {
@@ -20,11 +27,35 @@ const Movie = (props) => {
         console.log(e);
       });
   };
- 
+
   useEffect(() => {
     getMovie(props.match.params.id);
   }, [props.match.params.id]);
-  return (<div>
-    
-  </div>);
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Image src={movie.poster + "/100px250"} fluid />
+          </Col>
+
+          <Col>
+            <Card>
+              <Card.Header as="h5">{movie.title}</Card.Header>
+              <Card.Body>
+                <Card.Text>{movie.plot}</Card.Text>
+                {props.user && (
+                  <Link to={"/movies/" + props.match.params.id + "/review"}>
+                    Add Review
+                  </Link>
+                )}
+              </Card.Body>
+            </Card>
+            <br></br>
+            <h2>Reviews</h2>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
