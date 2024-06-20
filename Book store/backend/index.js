83f7d24f371533c3f5d24fd937ab6221f6f2f767
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-06-20 21:10:04 +0200

    Mern

--- Book store/backend/index.js
+++ Book store/backend/index.js
@@ -1,9 +1,12 @@
 import express from "express";
-
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
 
 app.get("/", (req, res) => {
     console.log(req);
