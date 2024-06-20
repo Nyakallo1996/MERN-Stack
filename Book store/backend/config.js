326af11b3ec329f856d211cc660fd7978d6dedf0
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-06-20 21:18:54 +0200

    Mern

--- Book store/backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://lekgoabamahlaks:BiTMfUuoUOt3so1x@bookstore.mev74jh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore'
---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://lekgoabamahlaks:BiTMfUuoUOt3so1x@bookstore.mev74jh.mongodb.net/?retryWrites=true&w=majority&appName=BookStore";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
