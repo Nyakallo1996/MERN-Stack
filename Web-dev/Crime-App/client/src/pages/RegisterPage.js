dc0e2803e4a6e1807b5219ea389b25d688efd5c3
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-08-16 21:57:45 +0200

    small changes

--- Web-dev/Crime-App/client/src/pages/RegisterPage.js
+++ Web-dev/Crime-App/client/src/pages/RegisterPage.js
@@ -2,6 +2,7 @@ export default function RegisterPage() {
   return (
     <div>
       <form className="register">
+        <h1>Register</h1>
         <input type="text" placeholder="username" />
         <input type="password" placeholder="password" />
         <button>Register</button>
