8efafc269403cbe5fab94060cb5dae78233680b7
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-08-17 20:54:50 +0200

    small changes

--- Web-dev/RECIPE_APP/server/src/index.js
+++ Web-dev/RECIPE_APP/server/src/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import {userRouter} from "./routes/user.js"
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect(
   "mongodb+srv://mahlakametsakabelo:W7YFDwaWlZ6x6ccH@crimeapp.nfzldo7.mongodb.net/?retryWrites=true&w=majority&appName=Crimeapp"
--- Web-dev/RECIPE_APP/server/src/models/User.js
+++ Web-dev/RECIPE_APP/server/src/models/User.js
@@ -4,3 +4,5 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true},
     password: { type: String, required: true}
 });
+
+export const UserModel = mongoose.model("users", UserSchema);
