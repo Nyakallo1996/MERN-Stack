984ec51b5795559d6dfe5b9c4d549e4ed2c3b1ec
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-08-18 11:21:20 +0200

    small changes

--- Web-dev/RECIPE_APP/server/src/index.js
+++ Web-dev/RECIPE_APP/server/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
 
-import {userRouter} from "./routes/user.js"
+
 
 const app = express();
 
@@ -10,9 +12,14 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(
-  "mongodb+srv://mahlakametsakabelo:W7YFDwaWlZ6x6ccH@crimeapp.nfzldo7.mongodb.net/?retryWrites=true&w=majority&appName=Crimeapp"
+  "mongodb+srv://mahlakametsakabelo:W7YFDwaWlZ6x6ccH@crimeapp.nfzldo7.mongodb.net/?retryWrites=true&w=majority&appName=Crimeapp",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 app.listen(3001, () => console.log("SERVER STARTED"));
--- Web-dev/RECIPE_APP/server/src/routes/user.js
+++ Web-dev/RECIPE_APP/server/src/routes/user.js
@@ -1,25 +1,20 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-
 import bcrypt from "bcrypt";
-import { UserModel } from "../models/User.js";
 
 const router = express.Router();
+import { UserModel } from "../models/Users.js";
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const user = await UserModel.findOne({ username: username });
-
+  const user = await UserModel.findOne({ username });
   if (user) {
-    return res.json({ message: "User already exists!" });
+    return res.status(400).json({ message: "Username already exists" });
   }
-
   const hashedPassword = await bcrypt.hash(password, 10);
-
   const newUser = new UserModel({ username, password: hashedPassword });
   await newUser.save();
-
-  res.json({ message: "User Registered Successfully!" });
+  res.json({ message: "User registered successfully" });
 });
 
 router.post("/login", async (req, res) => {
@@ -43,3 +38,17 @@ router.post("/login", async (req, res) => {
 });
 
 export { router as userRouter };
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secret", (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
